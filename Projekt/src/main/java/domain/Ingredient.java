8fd01877d366a5aec605e3b6b49e555337a88fe4
Marta Walczak <mwalczak@sigma.ug.edu.pl>
2015-11-08 02:08:19 +0100

    Dodanie wnetrz klas Cake i Ingredient

--- Projekt/src/main/java/domain/Ingredient.java
+++ Projekt/src/main/java/domain/Ingredient.java
@@ -1,5 +1,36 @@
 package domain;
 
 public class Ingredient {
-
+	
+	private int id;
+	
+	private String name;
+	private String kind;
+	
+	public Ingredient() {
+	}
+	public Ingredient(String name, String kind) {
+		super();
+		this.name = name;
+		this.kind = kind;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getKind() {
+		return kind;
+	}
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
 }
