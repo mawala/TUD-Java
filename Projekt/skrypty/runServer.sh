4632620ddd0fd0f71e0a73fc1d5c25f896153f51
Marta Walczak <mwalczak@sigma.ug.edu.pl>
2015-11-12 21:44:37 +0100

    poprawa skryptow

--- Projekt/skrypty/runServer.sh
+++ Projekt/skrypty/runServer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp .\.m2\repository\org\hsqldb\hsqldb\2.2.4\hsqldb-2.2.4.jar org.hsqldb.server.Server --database.0 mem:mydb --dbname.0 workdb
+java -cp ..\jar\hsqldb-2.2.4.jar org.hsqldb.server.Server --database.0 mem:mydb --dbname.0 workdb
