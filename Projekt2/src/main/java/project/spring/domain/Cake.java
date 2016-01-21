0e6128f7d3ae7d8b072c39be02d031a6592b936b
Marta Walczak <mwalczak@sigma.ug.edu.pl>
2016-01-21 14:33:11 +0100

    Dodanie katalogu domain w src/main

---
+++ Projekt2/src/main/java/project/spring/domain/Cake.java
@@ -0,0 +1,51 @@
+package project.spring.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+@Entity
+@NamedQueries({
+	@NamedQuery(name = "Cake.getAll", query = "SELECT c FROM Cake c")
+})
+public class Cake {
+	
+	private Long id;
+	private Double price;
+	private Double weight;
+		
+	public Cake(){
+	}
+	public Cake(Double price, Double weight){
+		super();
+		this.price = price;
+		this.weight = weight;
+	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	public Double getName() {
+		return weight;
+	}
+
+	public void setName(Double weight) {
+		this.weight = weight;
+	}
+}
---
+++ Projekt2/src/main/java/project/spring/domain/Relationship.java
@@ -0,0 +1,31 @@
+package project.spring.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Relationship {
+
+	private long cakeId;
+	private long ingredientId;
+	
+	public Relationship() {
+	}
+	public Relationship(long cId, long iId) {
+		super();
+		this.cakeId = cId;
+		this.ingredientId = iId;
+	}
+	
+	public long getCakeId() {
+		return cakeId;
+	}
+	public void setCakeId(long cakeId) {
+		this.cakeId = cakeId;
+	}
+	public long getIngredientId() {
+		return ingredientId;
+	}
+	public void setIngredientId(long ingredientId) {
+		this.ingredientId = ingredientId;
+	}
+}
